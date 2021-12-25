62dcfe943bcf9c58a3c617cafcb55ec43426653d
Mackmiller <mackmiller19@gmail.com>
2021-12-25 18:35:53 -0500

    fixed server bug within get routes

--- app/routes/coinCapApi.js
+++ app/routes/coinCapApi.js
@@ -3,26 +3,26 @@ const express = require('express')
 const axios = require('axios')
 const router = express.Router()
 
-// config for api call
-var config = {
-    method: 'GET',
-    url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/category?id=6051a82566fc1b42617d6dc6&limit=10',
-    qs: {
-        'start': '1',
-        'limit': '10',
-        'convert': 'USD'
-      },
-    headers: {
-        "X-CMC_PRO_API_KEY": `${process.env.API_KEY}`
-    },
-    json: true
-}
 // GET all meme coins
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
+    // config for api call
+    var config = {
+        method: 'GET',
+        url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/category?id=6051a82566fc1b42617d6dc6&limit=10',
+        qs: {
+            'start': '1',
+            'limit': '10',
+            'convert': 'USD'
+        },
+        headers: {
+            "X-CMC_PRO_API_KEY": `${process.env.API_KEY}`
+        },
+        json: true
+    }
     axios(config)
         .then(function(response) {
             // entire data object for meme category
-            console.log(res.json(response.data))
+            return(res.json(response.data))
 
             // specifically the coins data within the meme category
             // console.log(res.json(response.data.data.coins))
@@ -34,25 +34,20 @@ router.get('/', (req, res, next) => {
 })
 
 // GET metadata for specific coin (id)
-router.get('/cryptocoin/:id', (req, res, next) => {
+router.get('/cryptocoin/:id', (req, res) => {
     var config = {
         method: 'GET',
         url: `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=${req.params.id}`,
-        qs: {
-            'start': '1',
-            'limit': '10',
-            'convert': 'USD'
-          },
         headers: {
             "X-CMC_PRO_API_KEY": `${process.env.API_KEY}`
         },
         json: true
     }
-    console.log('coin selected: ', req.params.id)
+    // console.log('coin selected: ', req.params.id)
     axios(config)
         .then(function(response) {
-            console.log('This is the individual coin metadata', res.json(response.data))
-            console.log(res.json(response.data))
+            // console.log('This is the individual coin metadata', res.json(response.data))
+            return(res.json(response.data))
         })
         .catch(function (error) {
             console.log(error);
